cefb1a6c7d09d89e7608f24ed1b27fd102308a2d
Brian Bickes <brianbickes@Brians-MacBook-Pro.local>
2020-08-03 16:07:37 -0700

    MVP complete. Spaced out rink and buttons

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 //Stats for the user and the goalie. For right now, only have power go against save. User will always win.
-
 const user = { name: 'Connor McDavid', power: 100, points: 0 };
 const goalie = [
     { name: 'Craig Anderson', save: 70, points: 0 },
     { name: 'Marc-Andre Fleury', save: 75, points: 0 },
     { name: 'John Gibson', save: 80, points: 0 },
     { name: 'Pekka Rinne', save: 85, points: 0 },
-]
-
-// shotType = 
-//     {
-//         foreHand: 'f'
-//     },
-//     {
-//         backHand: 'h'
-//     }
-// ];
+];
 console.log(user);
-console.log(goalie[0])
-
+console.log(goalie)
 //Beginning of the game. Player will be directed here and will make a choice of whether or not to enter the shootout. 
 
 const onLoad = () => {
@@ -34,43 +23,43 @@ userChoice = () => {
         shootOut();
     } else (console.log('NO'));
 };
-// const $div = $("<div>").attr("id", "userPoints")
-// $div.html(`<h2>${user.points}</h2>`)
-// $('body').append($div);
 
-
+checkWin = () => {
+    if (goalie.length === 0) {
+        userWin();
+    } else {
+        shootOut();
+    }
+}
 //Shootout function to determine who gets points. 
 const shootOut = () => {
-    if (user.power + Math.random() * 20 > goalie[0].save + Math.random() * 15) {
+    if (user.power + Math.floor(Math.random() * 20) > goalie[0].save + Math.floor(Math.random() * 15)) {
         user.points++;
         alert(`You just scored on ${goalie[0].name}!. You have ${user.points} goal and ${goalie[0].name} has made ${goalie[0].points} saves.`)
-    } if (user.power + Math.random() * 20 < goalie[0].save + Math.random() * 15) {
+    } if (user.power + Math.floor(Math.random() * 20) < goalie[0].save + Math.floor(Math.random() * 15)) {
         goalie[0].points++;
         alert(`${goalie[0].name} just made a save and they have ${goalie[0].points} and you have ${user.points}`);
-    } else {
-        alert('The shot missed! No points are awarded.' || 'It hit the post!');
     };
-    if (user.points <= 3 && goalie[0].points <= 3) {
+    if (user.points < 4 && goalie[0].points < 4) {
         shootOut();
-    } if (user.points === 4 && goalie[0].points < 4) {
+    } if (user.points === 4 && goalie[0].points < 4 && goalie.length !== 0) {
+        checkWin();
         const choice = prompt(`You beat ${goalie[0].name}! Would you like to keep playing? The next goalie is much better.`, 'Yes');
         if (choice === 'Yes') {
             //shifts goalie to the next in the array. Next goalie is more difficult than the previous.
             goalie.shift();
             user.points = 0;
-            if(goalie.length===0){
-                userWin();
-            }
-            shootOut();
-        } if (user.points === 4 && goalie.points < 4 && goalie.length === 0) {
-            userWin();
-        } if (user.point <= 4 && goalie.points === 4 && goalie.lenth === 0) {
-            goalieWin();
-        } else if (goalie.length === 0) {
-            goalieWin();
+            checkWin();
         }
-    };
+    } if (user.points === 4 && goalie.points < 4 && goalie.length === 0) {
+        userWin();
+    } if (user.point < 4 && goalie.points === 4) {
+        goalieWin();
+    }
 };
+// const $div = $("<div>").attr("id", "userPoints")
+// $div.html(`<h2>${user.points}</h2>`)
+// $('body').append($div);
 
 const userWin = () => {
     alert('You won the shootout! Celebrate with your teammates');
@@ -78,24 +67,18 @@ const userWin = () => {
     if (choice.toLowerCase().trim() === 'y' || 'yes' || 'Yes' || 'Y') {
         user.points = 0;
         goalie.unshift();
-        goalie.unshift(); 
         goalie.unshift();
-        // goalie[0].points=0; 
-        // goalie[1].points=0; 
-        // goalie[2].points=0; 
-        // goalie[3].points=0; 
+        goalie.unshift();
+        goalie.unshift();
+        goalie[0].points = 0;
+        goalie[1].points = 0;
+        goalie[2].points = 0;
+        goalie[3].points = 0;
         onLoad();
-    }
+    } 
 };
 
 const goalieWin = () => {
     alert('You lost');
 };
 
-// endGame = () => {
-//     if(user )
-// }
-
-// while (user.points < 4 && goalie.points < 4) {
-
-// }
