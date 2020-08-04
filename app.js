4493862e4a425e8f7310498682dbc547df9973d4
Brian Bickes <brianbickes@Brians-MacBook-Pro.local>
2020-08-04 15:02:10 -0700

    Moved text from alerts to show inside html

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ const goalie = [
 
 const round = [1, 2, 3, 4];
 
-console.log(user);
-console.log(goalie)
-//Beginning of the game. Player will be directed here and will make a choice of whether or not to enter the shootout. 
+const $div = $("#textBox");
+    $h3 = $('#text');
+    $div.append($h3);
 
+// console.log(user);
+// console.log(goalie);
+$h3.text('Click Start to begin the game.')
+//Beginning of the game. Player will be directed here and will make a choice of whether or not to enter the shootout. 
 const onLoad = () => {
-    const choice = prompt(`Hello there ${user.name}! Your shot power is ${user.power}. Type 's' to start.`, 's');
-    if (choice.toLowerCase().trim() === 's') {
-        userChoice();
-    };
+    $h3.text('Hello there! Your number has been called and you are set to face off against a series of goalies in the shootout. Click shoot when you are ready.');
 };
 //User prompt for the start of the shootout. 
 userChoice = () => {
-    const choice = prompt(`Coach has asked you to step in. The goalie you are facing is ${goalie.name}. Are you ready?`, 'Y');
+    const choice = prompt(`Coach has asked you to step in. The goalie you are facing is ${goalie[0].name}. Are you ready?`, 'Y');
     if (choice.toLowerCase().trim() === 'y' || 'yes' || 'Yes' || 'Y') {
         alert('Click shoot to take your shot');
     } else {
@@ -36,41 +37,41 @@ checkWin = () => {
         shootOut();
     }
 }
-$scoreBoard = $('#scoreboard');
-
 
 const userWin = () => {
     alert('You won the shootout! Celebrate with your teammates');
     const choice = alert('Click the "Restart Game" button to play again.');
-    };
+};
 
 const goalieWin = () => {
     alert('You lost');
 };
 
-
 //Shootout function to determine who gets points. 
 const shootOut = () => {
     if (user.power + Math.floor(Math.random() * 30) > goalie[0].save + Math.floor(Math.random() * 15)) {
         user.points++;
-        alert(`You just scored on ${goalie[0].name}!. You have ${user.points} goal and ${goalie[0].name} has made ${goalie[0].points} saves.`);
+        $h3.text(`You just scored on ${goalie[0].name}!. You have ${user.points} goal and ${goalie[0].name} has made ${goalie[0].points} saves.`);
     } if (user.power + Math.floor(Math.random() * 30) < goalie[0].save + Math.floor(Math.random() * 15)) {
         goalie[0].points++;
-        alert(`${goalie[0].name} just made a save and they have ${goalie[0].points} and you have ${user.points}`);
+        $h3.text(`Ouch! ${goalie[0].name} just robbed you. Get ready to take another shot.`);
     };
-    if (user.points < 4 && goalie[0].points < 4) {
-            alert('click shoot to take your next shot');
-    } if (user.points === 4 && goalie[0].points < 4 && goalie.length !== 0) {
+    // if (user.points < 4 && goalie[0].points < 4) {
+    //     $h3.text('click shoot to take your next shot');
+    // } 
+    if (user.points === 4 && goalie[0].points < 4 && goalie.length !== 0) {
         const choice = prompt(`You beat ${goalie[0].name}! Would you like to keep playing? The next goalie is much better.`, 'Yes');
         if (choice === 'Yes') {
             //shifts goalie to the next in the array. Next goalie is more difficult than the previous.
             goalie.shift();
             round.shift();
             user.points = 0;
+        } else {
+            alert("That's ok. Take a rest and click the shoot button when you are ready.")
         }
-    } if (user.points === 4 && goalie.points < 4 && goalie.length === 0) {
+    } if (user.points === 4 && goalie[0].points < 4 && goalie.length === 0) {
         userWin();
-    } if (user.points < 4 && goalie.points === 4) {
+    } if (user.points < 4 && goalie[0].points === 4) {
         goalieWin();
     }
 
